18ebc9803664adf354211953b19dc32028c7c148
Erzhan Zhusupov <zzhusupoverzhan@gmail.com>
2025-08-08 20:47:15 -0400

    fifth

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,20 @@
-# 1. Базовый образ (например, Python 3.11 slim)
+
 FROM python:3.11-slim
 
-# 2. Рабочая директория в контейнере
+
 WORKDIR /bloglite
 
-# 3. Копируем файл зависимостей
+
 COPY requirements.txt .
 
-# 4. Устанавливаем зависимости
+
 RUN pip install --no-cache-dir -r requirements.txt
 
-# 5. Копируем всё остальное
+
 COPY . .
 
-# 6. Открываем порт (если нужно)
+
 EXPOSE 8000
 
-# 7. Команда запуска
+
 CMD ["python", "bloglite/manage.py", "runserver", "0.0.0.0:8000"]
--- bloglite/bloglite/settings.py
+++ bloglite/bloglite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -82,8 +83,12 @@ WSGI_APPLICATION = 'bloglite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST', 'db'),  
+        'PORT': os.getenv('POSTGRES_PORT', '5432'),
     }
 }
 
